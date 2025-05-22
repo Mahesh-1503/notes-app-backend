d87352765040c095fb621b6eac52af2b50409e01
Mahesh-1503 <mnopxyz090@gmail.com>
2025-05-22 11:18:22 +0530

    token solved

--- controllers/authController.js
+++ controllers/authController.js
@@ -101,3 +101,17 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+// Get current user
+exports.getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error("Get current user error:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
